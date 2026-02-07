57ee1acd8e790510ee6835505204bf97cff82ddb
med-elimam <moh2181106@maricopa.edu>
2026-02-07 19:59:44 +0000

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for expo-sqlite web build
+config.resolver.assetExts.push('wasm');
+
 module.exports = config;
